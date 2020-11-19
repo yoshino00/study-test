91b636200fd92e709a56c98b65a290ac227cdc31
yoshino <msg1801022@gmail.com>
2020-11-19 15:42:25 +0900

    usercontroller.phpの追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return view('users.index', ['users' => $users]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('users', 'App\Http\Controllers\UserController');
+Route::resource('/users', 'App\Http\Controllers\UserController');
